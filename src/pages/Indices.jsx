b4e9d72a57f8e7fef2c53c2db97326f11ae7c81b
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 15:59:59 +0530

    Removed IC Market

--- src/pages/Indices.jsx
+++ src/pages/Indices.jsx
@@ -1,4 +1,3 @@
-
 import Banner from "../components/Banner";
 import { assets } from "../assets/assets";
 import IndicesFactsSection from "../components/IndicesFactsSection";
@@ -10,8 +9,8 @@ export default function Indices() {
   return (
     <div>
       <Banner
-        title="Indices"
-        description="The IC Markets Global Indices offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips."
+        title="Indices "
+        description="The KiteFX  Global Indices offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips. "
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
