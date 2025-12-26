256151c00e360c7b4b3f424104f49f2c02d15a60
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 15:54:22 +0530

    Removed IC Market

--- src/pages/CTraderRawSpread.jsx
+++ src/pages/CTraderRawSpread.jsx
@@ -8,8 +8,8 @@ export default function CTrader() {
   return (
     <div>
       <Banner
-        title="cTrader Raw Spread Account"
-        description="cTrader was built for demanding performances and, when combined with IC Markets Global Raw pricing and unrivalled execution speeds, cTrader makes a valuable addition to your trading arsenal and will help take your trading to the next level."
+        title="cTrader Raw Spread Account "
+        description="cTrader was built for demanding performances and, when combined with KiteFX Global Raw pricing and unrivalled execution speeds, cTrader makes a valuable addition to your trading arsenal and will help take your trading to the next level."
         buttonText="Open account"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
