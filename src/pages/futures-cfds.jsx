94bf5869e42a0cbf69c8c80be54cadf830858610
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:04:19 +0530

    Removed IC Market

--- src/pages/futures-cfds.jsx
+++ src/pages/futures-cfds.jsx
@@ -1,4 +1,3 @@
-
 import Banner from "../components/Banner";
 import { assets } from "../assets/assets";
 import FuturesFactsSection from "../components/FuturesFactsSection";
@@ -6,25 +5,20 @@ import FuturesCFDExample from "../components/FuturesCFDExample";
 import UpcomingExpiringMarkets from "../components/UpcomingExpiringMarkets";
 import CommoditiesExpiryTable from "../components/CommoditiesExpiryTable";
 
-
-
-
 export default function FuturesCFDs() {
   return (
     <div>
       <Banner
-        title="Futures CFDs"
-        description="The IC Markets Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips."
+        title="Futures CFDs "
+        description="The KiteFX  Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips. "
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
       />
-     <FuturesFactsSection/>
-     <FuturesCFDExample/>
-     <UpcomingExpiringMarkets/>
-     <CommoditiesExpiryTable/>
-    
-      
+      <FuturesFactsSection />
+      <FuturesCFDExample />
+      <UpcomingExpiringMarkets />
+      <CommoditiesExpiryTable />
     </div>
   );
 }
