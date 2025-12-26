9b577b20f26b6cd54290ebfb9a63c92b635420f8
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:07:45 +0530

    Removed IC Market

--- src/pages/TradingConditions.jsx
+++ src/pages/TradingConditions.jsx
@@ -7,8 +7,8 @@ const TradingConditions = () => {
   return (
     <div>
       <Banner
-        title="Trading Conditions"
-        description="The IC Markets Global Trading environment offers the most aggressive pricing and flexible trading conditions globally. They are ideal for traders who demand the best execution quality and lowest spreads."
+        title="Trading Conditions "
+        description="The KiteFX Global Trading environment offers the most aggressive pricing and flexible trading conditions globally. They are ideal for traders who demand the best execution quality and lowest spreads. "
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
