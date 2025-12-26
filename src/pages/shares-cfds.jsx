ad5894f7b86916f1844fc3178a9008783f2d939a
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:03:44 +0530

    Removed IC Market

--- src/pages/shares-cfds.jsx
+++ src/pages/shares-cfds.jsx
@@ -10,8 +10,8 @@ export default function StocksCFDs() {
   return (
     <div>
       <Banner
-        title="Stocks CFDs "
-        description="The KiteFX  Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips. "
+        title="Stocks CFDs"
+        description="The KiteFX  Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips."
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
