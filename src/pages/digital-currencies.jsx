f300daf0d2c60a1db23ff6a14ad1c5090c1ee55c
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:00:40 +0530

    Removed IC Market

--- src/pages/digital-currencies.jsx
+++ src/pages/digital-currencies.jsx
@@ -1,4 +1,3 @@
-
 import Banner from "../components/Banner";
 import { assets } from "../assets/assets";
 import CryptoCFDFactsSection from "../components/CryptoCFDFactsSection";
@@ -6,13 +5,12 @@ import CryptoCFDsGrid from "../components/CryptoCFDsGrid";
 import HowCryptoCFDWorks from "../components/HowCryptoCFDWorks";
 import BitcoinCFDTradingExample from "../components/BitcoinCFDTradingExample";
 
-
 export default function DigitalCurrencies() {
   return (
     <div>
       <Banner
-        title="Digital Currencies"
-        description="The IC Markets Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips."
+        title="Digital Currencies "
+        description="The KiteFX  Global Digital Currencies offering is one of the most competitive in the world. Access the world’s largest and most liquid market with Raw spreads starting from 0.0 pips. "
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
