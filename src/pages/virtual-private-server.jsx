b667cbeb07d71ae4488f2eac175483bc8cd9ed1c
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:20:36 +0530

    Removed IC Market

--- src/pages/virtual-private-server.jsx
+++ src/pages/virtual-private-server.jsx
@@ -10,8 +10,8 @@ export default function VirtualPrivateServer() {
   return (
     <div>
       <Banner
-        title="Virtual Private Server "
-        description="Execute automated trading strategies 24 hours a day, 7 days a week with the fastest possible connectivity to the KiteFX  Global trading servers. "
+        title="Virtual Private Server"
+        description="Execute automated trading strategies 24 hours a day, 7 days a week with the fastest possible connectivity to the KiteFX  Global trading servers."
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
