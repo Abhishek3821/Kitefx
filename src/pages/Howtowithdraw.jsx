5294590620030827448fb9ccc72eddc41ac4d141
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 15:53:09 +0530

    Removed IC Market

--- src/pages/Howtowithdraw.jsx
+++ src/pages/Howtowithdraw.jsx
@@ -6,8 +6,8 @@ export default function YourAccount() {
   return (
     <div>
       <Banner
-        title="Funds Withdrawal"
-        description="For faster processing all account holders are now required to submit withdrawal requests from inside their Secure Client Area. If you do not yet have access to your Secure Client Area please follow the Access Instructions."
+        title="Funds Withdrawal "
+        description="For faster processing all account holders are now required to submit withdrawal requests from inside their Secure  Client Area. If you do not yet have access to your Secure Client Area please follow the Access Instructions."
         buttonText="Withdrawal Form"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
