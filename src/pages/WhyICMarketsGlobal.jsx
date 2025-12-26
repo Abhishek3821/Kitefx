3ec35b5e4f4db06d852c5402db4b045aa9d2f2d2
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 15:41:42 +0530

    Removed IC Market

--- src/pages/WhyICMarketsGlobal.jsx
+++ src/pages/WhyICMarketsGlobal.jsx
@@ -8,8 +8,8 @@ export default function Quickstart() {
   return (
     <div>
       <Banner
-        title="Why Choose IC Markets Global"
-        description="IC Markets Global has grown to become one of the largest MetaTrader 4 Forex CFD providers in the world by giving traders what they really want."
+        title="Why Choose KiteFX  Global"
+        description="KiteFX Global has grown to become one of the largest MetaTrader 4 Forex CFD providers in the world by giving traders what they really want."
         buttonText="Start Trading"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
