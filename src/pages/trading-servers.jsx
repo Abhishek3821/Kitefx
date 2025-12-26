a3dfadad3dc04797fddbb6d873effae1b4470228
Abhishek3821 <abhisheksinghchauhan97@gmail.com>
2025-12-26 16:21:16 +0530

    Removed IC Market

--- src/pages/trading-servers.jsx
+++ src/pages/trading-servers.jsx
@@ -6,8 +6,8 @@ export default function TradingServers() {
   return (
     <div>
       <Banner
-        title="Trading Servers "
-        description="KiteFX Global trading infrastructure is collocated in the Equinix NY4 Data Centre with our prices and technology partners to give you  best execution and the low latency pricing possible. "
+        title="Trading Servers"
+        description="KiteFX Global trading infrastructure is collocated in the Equinix NY4 Data Centre with our prices and technology partners to give you  best execution and the low latency pricing possible."
         buttonText="Open account"
         onButtonClick={() => console.log("CTA clicked")}
         backgroundImage={assets.kitefx20}
